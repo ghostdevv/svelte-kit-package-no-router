79a0895a33083637c83c48ae0a788fe7cf264c79
GHOST <ghostdevbusiness@gmail.com>
2022-01-06 11:27:44 +0000

    initial commit

---
+++ src/Counter.svelte
@@ -0,0 +1,5 @@
+<script>
+    let count;
+</script>
+
+<button on:click={() => (count += 1)}> Click Me </button>
---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: preprocess(),
+
+    kit: {
+        files: {
+            lib: 'src',
+        },
+    },
+};
+
+export default config;
